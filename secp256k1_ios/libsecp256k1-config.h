fd74e103d8549e36354bf850eb9fcff178491bc2
Alex Vlasov <shamatar@github.com>
2018-01-08 17:59:30 +0300

    fix int128

--- secp256k1_ios/libsecp256k1-config.h
+++ secp256k1_ios/libsecp256k1-config.h
@@ -16,6 +16,8 @@
 #define USE_ECMULT_STATIC_PRECOMPUTATION 1
 #define ENABLE_MODULE_RECOVERY 1
 #ifdef __LP64__
+typedef __int128 int128_t;
+typedef unsigned __int128 uint128_t;
 #define HAVE___INT128 1
 #define USE_FIELD_5X52 1
 #define USE_SCALAR_4X64 1
